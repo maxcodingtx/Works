294ee2b73ee9c4c497dea17a0d1cf41e80d78a12
maxcodingtx <maxcodingtx@gmail.com>
2025-01-28 21:49:27 -0600

    added interactivity to nav image.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
 
 
-const contactButton = document.getElementById('contactButton')
-const contactDropdownContent = document.getElementById('contactDropdownContent')
+const contactButton = document.getElementById('contactButton');
+const contactDropdownContent = document.getElementById('contactDropdownContent');
+
 contactButton.addEventListener('click', () => {
-    contactDropdownContent.classList.toggle('is-active--contact')
-})
+    // making links visible or hiding them by toggling active class (display: block;)
+    contactDropdownContent.classList.toggle('is-active--contact');
+
+    const buttonImage = document.getElementById('contactImage')
+    // if links are currently shown, flip the image inside the contact button
+    if (contactDropdownContent.classList.contains('is-active--contact')) {
+        buttonImage.style.transform = 'rotate(180deg)'
+    }
+    // if links are not currently shown, keep image in natural position
+    else {
+        buttonImage.style.transform = 'rotate(0)'
+    }
+
+});
