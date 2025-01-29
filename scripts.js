43a5b40d16fc7445513153119604c562e11af3e7
maxcodingtx <maxcodingtx@gmail.com>
2025-01-29 13:11:53 -0600

    re-worked scripts to work for both nav elements

--- scripts.js
+++ scripts.js
@@ -1,20 +1,34 @@
 
 
-const contactButton = document.getElementById('contactButton');
-const contactDropdownContent = document.getElementById('contactDropdownContent');
+const contactButtonSelector = document.querySelectorAll('.dropdown__button');
+const contactDropdownContent = document.querySelectorAll('dropdown__content');
 
-contactButton.addEventListener('click', () => {
-    // making links visible or hiding them by toggling active class (display: block;)
-    contactDropdownContent.classList.toggle('is-active--contact');
+// window event listener if both navs use same 'is-active' class
 
-    const buttonImage = document.getElementById('contactImage')
-    // if links are currently shown, flip the image inside the contact button
-    if (contactDropdownContent.classList.contains('is-active--contact')) {
-        buttonImage.style.transform = 'rotate(180deg)'
-    }
-    // if links are not currently shown, keep image in natural position
-    else {
-        buttonImage.style.transform = 'rotate(0)'
-    }
+
+contactButtonSelector.forEach((contactButtonSelector) => {
+    contactButtonSelector.addEventListener('click', (event) => {
+        // current button id's last character (integer)
+        var buttonIdInt = event.currentTarget.id[event.currentTarget.id.length-1]
+
+
+        // using the current button's id to select appropriate drop down
+        let targetDropdown = document.getElementById(`contactDropdownContent${buttonIdInt}`)
+        // making dropdown visible or hiding them by toggling active class (display: block;)
+        targetDropdown.classList.toggle('is-active')
+
+        // using the current button's id integer to select appropriate image
+        let targetImage = document.getElementById(`contactImage${buttonIdInt}`)
+        
+        // if links are currently shown, flip the image inside the contact button
+        if (targetDropdown.classList.contains('is-active')) {
+            targetImage.style.transform = 'rotate(180deg)'
+        }
+        // if links are not currently shown, keep image in natural position
+        else {
+            targetImage.style.transform = 'rotate(0)'
+        }
 
 });
+})
+
